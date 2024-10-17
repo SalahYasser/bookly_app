deaaca106dbb6bb0a06fe546461a4f25a0daec5d
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-10-17 17:29:40 +0300

    refactor books action

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,8 +1,8 @@
 import 'package:bookly/Features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -49,38 +49,3 @@ class BookDetailsViewBody extends StatelessWidget {
   }
 }
 
-class BooksAction extends StatelessWidget {
-  const BooksAction({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      child: Row(
-        children: [
-          Expanded(
-            child: CustomButton(
-              backgroundColor: Colors.white,
-              textColor: Colors.black,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16),
-                bottomLeft: Radius.circular(16),
-              ), text: '19.99 €',
-            ),
-          ),
-          Expanded(
-            child: CustomButton(
-              fontSize: 16,
-              backgroundColor: Color(0xffEF8262),
-              textColor: Colors.white ,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(16),
-                bottomRight: Radius.circular(16),
-              ), text: 'Free preview',
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
