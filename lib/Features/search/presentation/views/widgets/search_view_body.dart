040f29f581ed2a9141677ecd426508dd195ebd3c
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-10-19 02:16:11 +0300

    create book model

--- lib/Features/search/presentation/views/widgets/search_view_body.dart
+++ lib/Features/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly/Features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:bookly/core/utils/styles.dart';
-import 'package:bookly/core/widgets/custom_search_text_field.dart';
+import 'package:bookly/Features/search/presentation/views/widgets/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
