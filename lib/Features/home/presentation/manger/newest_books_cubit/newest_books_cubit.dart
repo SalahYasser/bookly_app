677e53fbff1f47964194535a8fad086a0d7a963e
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-10-19 17:59:23 +0300

    newest books cubit done

--- lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -1,10 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
 
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    var result = await homeRepo.fetchNewestBooks();
+
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
 }
