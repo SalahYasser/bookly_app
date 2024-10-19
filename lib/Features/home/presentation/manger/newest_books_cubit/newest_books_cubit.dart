6c5ef8b5f7ae6de84d152cb45ffa6b8e972b08a2
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-10-19 17:58:48 +0300

    create newest books cubit states

---
+++ lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+
+}
