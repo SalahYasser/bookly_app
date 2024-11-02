99e12c8cac194f1163b61661501e73b9a478a523
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-03 01:01:22 +0200

    add pagination statements to featured books list view

--- lib/Features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,25 +1,64 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class FeaturedBooksListView extends StatelessWidget {
+class FeaturedBooksListView extends StatefulWidget {
   const FeaturedBooksListView({super.key, required this.books});
 
   final List<BookEntity> books;
 
   @override
+  State<FeaturedBooksListView> createState() => _FeaturedBooksListViewState();
+}
+
+class _FeaturedBooksListViewState extends State<FeaturedBooksListView> {
+  late final ScrollController _scrollController;
+  var nextPage = 1;
+  var isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListen);
+  }
+
+  void _scrollListen() async {
+
+    var currentPositions = _scrollController.position.pixels;
+    var maxScrollLength = _scrollController.position.maxScrollExtent;
+
+    if (currentPositions >= 0.7 * maxScrollLength) {
+      if (!isLoading) {
+        isLoading = true;
+        await BlocProvider.of<FeaturedBooksCubit>(context)
+            .fetchFeaturedBooks(pageNumber: nextPage++);
+        isLoading = false;
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
+      height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
-        physics: const BouncingScrollPhysics(),
+        controller: _scrollController,
+        itemCount: widget.books.length,
         scrollDirection: Axis.horizontal,
-        itemCount: books.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
             child: CustomBookImage(
-              imageUrl: books[index].image ?? '',
+              imageUrl: widget.books[index].image ?? '',
             ),
           );
         },
@@ -27,4 +66,3 @@ class FeaturedBooksListView extends StatelessWidget {
     );
   }
 }
-
