bbdebd5683d57eff219b5f5612910d830efc265b
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-02 23:04:39 +0200

    implement methods for local data source

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -1,23 +1,36 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/constants.dart';
+import 'package:hive/hive.dart';
 
 abstract class HomeLocalDataSource {
 
   List<BookEntity> fetchFeaturedBooks();
 
   List<BookEntity> fetchNewestBooks();
+
+  List<BookEntity> fetchSimilarBooks();
 }
 
 class HomeLocalDataSourceImpl extends HomeLocalDataSource{
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+    return box.values.toList();
   }
 
   @override
   List<BookEntity> fetchNewestBooks() {
-    // TODO: implement fetchNewestBooks
-    throw UnimplementedError();
+
+    var box = Hive.box<BookEntity>(kNewestBox);
+    return box.values.toList();
+  }
+
+  @override
+  List<BookEntity> fetchSimilarBooks() {
+
+    var box = Hive.box<BookEntity>(kSimilarBox);
+    return box.values.toList();
   }
 
 
