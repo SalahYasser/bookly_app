b6972e784cbd25de943e8d92126d12d68882e0a9
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-02 22:35:07 +0200

    add Hive and generate BookEntity adapter

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,11 +1,22 @@
+import 'package:hive_flutter/adapters.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final int? pageCount;
+  @HiveField(5)
   final num? rating;
+  @HiveField(6)
   final String? categories;
+  @HiveField(7)
   final String? previewLink;
 
   BookEntity({
