36625679ff6305bf32ddb96937a8093ffb754d2d
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-03 01:45:56 +0200

    add pagination states to featured books list view bloc consumer

---
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_consumer.dart
@@ -0,0 +1,48 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:bookly/core/utils/helper_functions/custom_snack_bar.dart';
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:bookly/core/widgets/custom_loading_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FeaturedBooksListViewBlocConsumer extends StatefulWidget {
+  const FeaturedBooksListViewBlocConsumer({super.key});
+
+  @override
+  State<FeaturedBooksListViewBlocConsumer> createState() =>
+      _FeaturedBooksListViewBlocConsumerState();
+}
+
+class _FeaturedBooksListViewBlocConsumerState
+    extends State<FeaturedBooksListViewBlocConsumer> {
+  List<BookEntity> books = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<FeaturedBooksCubit, FeaturedBooksState>(
+      listener: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          books.addAll(state.books);
+        }
+        if (state is FeaturedBooksPaginationFailure) {
+          customSnackBar(context, state.errMessage);
+        }
+      },
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess ||
+            state is FeaturedBooksPaginationLoading ||
+            state is FeaturedBooksPaginationFailure) {
+          return FeaturedBooksListView(
+            books: books,
+          );
+        } else if (state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
+    );
+  }
+}
--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:bookly/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/featured_books_list_view_bloc_consumer.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/newest_list_view.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -19,7 +19,7 @@ class HomeViewBody extends StatelessWidget {
                 padding: EdgeInsets.symmetric(horizontal: 30),
                 child: CustomAppBar(),
               ),
-              FeaturedBooksListViewBlocBuilder(),
+              FeaturedBooksListViewBlocConsumer(),
               SizedBox(height: 50),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 30),
