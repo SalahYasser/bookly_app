445b2ffebc9754d763a6ac70ea9e08b117df96da
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-02 23:56:45 +0200

    create featured books list view bloc builder

--- lib/Features/home/presentation/views/widgets/featured_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view.dart
@@ -1,52 +1,26 @@
-import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_item.dart';
-import 'package:bookly/core/utils/app_router.dart';
-import 'package:bookly/core/widgets/custom_error_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
-      builder: (context, state) {
-        if (state is FeaturedBooksSuccess) {
-          return SizedBox(
-            height: MediaQuery.of(context).size.height * 0.3,
-            child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              scrollDirection: Axis.horizontal,
-              itemCount: state.books.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: GestureDetector(
-                    onTap: () {
-                      GoRouter.of(context).push(
-                        AppRouter.kBookDetailsView,
-                        extra: state.books[index],
-                      );
-                    },
-                    child: CustomBookImage(
-                      imageUrl:
-                          state.books[index].image ?? '',
-                    ),
-                  ),
-                );
-              },
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: CustomBookImage(
+              imageUrl: '',
             ),
           );
-        } else if (state is FeaturedBooksFailure) {
-          return CustomErrorWidget(
-            errMessage: state.errMessage,
-          );
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
+        },
+      ),
     );
   }
 }
+
---
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/Features/home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+class FeaturedBooksListViewBlocBuilder extends StatelessWidget {
+  const FeaturedBooksListViewBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FeaturedBooksListView();
+  }
+}
