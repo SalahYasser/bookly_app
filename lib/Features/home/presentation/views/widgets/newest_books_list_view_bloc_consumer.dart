75860c6077b00b8617408c8bfc4b97f02c2ba616
salahyasserallaithy <salahy.allaithy@gmail.com>
2024-11-03 02:27:14 +0200

    add pagination states to newest books list view bloc consumer

---
+++ lib/Features/home/presentation/views/widgets/newest_books_list_view_bloc_consumer.dart
@@ -0,0 +1,48 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/newest_books_list_view.dart';
+import 'package:bookly/core/utils/helper_functions/custom_snack_bar.dart';
+import 'package:bookly/core/widgets/custom_error_widget.dart';
+import 'package:bookly/core/widgets/custom_loading_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NewestBooksListViewBlocConsumer extends StatefulWidget {
+  const NewestBooksListViewBlocConsumer({super.key});
+
+  @override
+  State<NewestBooksListViewBlocConsumer> createState() =>
+      _FeaturedBooksListViewBlocConsumerState();
+}
+
+class _FeaturedBooksListViewBlocConsumerState
+    extends State<NewestBooksListViewBlocConsumer> {
+  List<BookEntity> books = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<NewestBooksCubit, NewestBooksState>(
+      listener: (context, state) {
+        if (state is NewestBooksSuccess) {
+          books.addAll(state.books);
+        }
+        if (state is NewestBooksPaginationFailure) {
+          customSnackBar(context, state.errMessage);
+        }
+      },
+      builder: (context, state) {
+        if (state is NewestBooksSuccess ||
+            state is NewestBooksPaginationLoading ||
+            state is NewestBooksPaginationFailure) {
+          return NewestBooksListView(
+            books: books,
+          );
+        } else if (state is NewestBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        } else {
+          return const CustomLoadingIndicator();
+        }
+      },
+    );
+  }
+}
